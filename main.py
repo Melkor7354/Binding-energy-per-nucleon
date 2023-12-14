e8b03f6ef56ccea63240bf5e1638ab87b5d679b9
Nazgul7354 <120272677+Melkor7354@users.noreply.github.com>
2023-12-15 04:29:12 +0530

    Final

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ primary = 'black'
 secondary = 'dark blue'
 text = 'white'
 font = ['Courier', 9]
+title = ['Courier', 13]
 mass_proton = 1.007277
 mass_neutron = 1.008665
 elements = []
@@ -28,7 +29,7 @@ class Interface(tk.Tk):
         self.title("Binding Energy per Nucleon Calculator")
         self.minsize(int(self.winfo_screenwidth()//2.5), int(self.winfo_screenheight()//2.5))
         self.maxsize(int(self.winfo_screenwidth()//2.5), int(self.winfo_screenheight()//2.5))
-        self.switch_frame(Calculator)
+        self.switch_frame(Menu)
 
     def switch_frame(self, frame_class):
         """Destroys current frame and replaces it with a new one."""
@@ -107,6 +108,7 @@ class Menu(tk.Frame):
         self.config(height=Interface.winfo_screenheight(self), width=Interface.winfo_screenwidth(self), bg=primary)
         MenuButton(display='Calculator', command=lambda: master.switch_frame(Calculator)).place(relx=0.4, rely=0.5)
         MenuButton(display='Graph', command=graph).place(relx=0.4, rely=0.7)
+        tk.Label(text="Binding Energy Calculator", bg=secondary, fg=text, font=title).place(relx=0.3, rely=0.2)
 
 
 class HomeButton(tk.Button):
@@ -148,7 +150,6 @@ def graph():
                 m = (int(j[0]) * mass_proton) + (((int(j[3]) - int((j[0]))) * mass_neutron) - float(j[4]))
                 be = m * 931.5
                 e = be / int(j[3])
-                print(e)
                 total += e
                 count += 1
                 total_mass += int(j[3])
@@ -157,14 +158,12 @@ def graph():
 
         average_be = total/count
         average_mass = total_mass/count
-        if average_be>0:
+        if average_be > 0:
             x.append(average_mass)
             y.append(average_be)
         else:
             pass
-    print(x)
-    print(y)
-    a = plt.plot(x, y, '-r')
+    plt.plot(x, y, '-r')
     plt.title(r'Variation of Binding Energy per Nucleon with Atomic Mass')
     plt.axhline(linewidth=2, color='grey')
     plt.axvline(linewidth=2, color='grey')
